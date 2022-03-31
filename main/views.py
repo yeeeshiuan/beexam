688ddbbec122de1f8b39f8bddb7b081a5c8cb56c
JoeyHuang <yeeeshiuan.huang@gmail.com>
2022-03-31 17:14:11 +0800

    [develop]     Add login_only to dashboard and profile

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'main/index.html', {'loadingMessage':''})
 
+@login_required
 def dashboard(request):
     return render(request, 'main/dashboard.html', {'loadingMessage':''})
 
+@login_required
 def profile(request):
     return render(request, 'main/profile.html', {'loadingMessage':''})

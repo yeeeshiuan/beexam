cc6a2f2b06fe169c3686081b76fabc447f506470
JoeyHuang <yeeeshiuan.huang@gmail.com>
2022-04-02 13:23:46 +0800

    [feature/20220401_fb_login]     test FB login SDK

--- main/views.py
+++ main/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from beexam.settings import env
 
 
 def index(request):
-    return render(request, 'main/index.html', {'loadingMessage':''})
+    return render(
+        request,
+        'main/index.html',
+        {
+            'loadingMessage':'',
+            'facebookId': env('FACEBOOK_APP_ID')
+        }
+    )
 
 @login_required(login_url='/')
 def dashboard(request):
-    return render(request, 'main/dashboard.html', {'loadingMessage':''})
+    return render(
+        request,
+        'main/dashboard.html',
+        {
+            'loadingMessage':'',
+            'facebookId': env('FACEBOOK_APP_ID')
+        }
+    )
 
 @login_required(login_url='/')
 def profile(request):
-    return render(request, 'main/profile.html', {'loadingMessage':''})
+    return render(
+        request,
+        'main/profile.html',
+        {
+            'loadingMessage':'',
+            'facebookId': env('FACEBOOK_APP_ID')
+        }
+    )

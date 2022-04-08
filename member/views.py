6e1e33c0f64d383058246905b7c573fceab47822
JoeyHuang <yeeeshiuan.huang@gmail.com>
2022-04-08 14:41:35 +0800

    [develop]     Remove useless import modules

--- member/views.py
+++ member/views.py
@@ -9,9 +9,7 @@ from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework.permissions import BasePermission, IsAuthenticated
 import json
 import urllib
